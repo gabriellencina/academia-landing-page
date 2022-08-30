761d09c2f3de812f211afa695f68011f31413b9d
Gabriel Lencina <gabriellencina831@gmail.com>
2022-08-30 17:01:09 -0300

    site finalizado

---
+++ scripts.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('p.about-academy',{
+     rotate: {x:0, y:80, z:0},
+    duration: 2000 
+});
+
+sr.reveal('#subscription-form',{
+    rotate: {x:0, y:80, z:0},
+   duration: 4000
+});
+
+sr.reveal('.benefit',{
+    rotate: {x:0, y:80, z:0},
+   duration: 4000 
+});
+
+sr.reveal('#address',{
+    rotate: {x:0, y:80, z:0},
+   duration: 2000 
+});
+
+sr.reveal('.localimg',{
+    rotate: {x:0, y:80, z:0},
+   duration: 4000 
+});
+
+sr.reveal('.academy-detail', {
+    rotate: {x:0, y:80, z:0},
+    duration: 4000
+});
+
+sr.reveal('.btn', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2000
+})
